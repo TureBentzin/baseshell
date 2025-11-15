c128aa112b6b066ec1f3f589c2dd24e4f1927783
Ture Bentzin <ture.bentzin@alumni.fh-aachen.de>
2025-11-15 18:41:14 +0000

    update: initial commit

---
+++ flake.nix
@@ -0,0 +1,94 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    let
+      version = "1.0.0";
+    in
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+      ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        let
+
+          shell = pkgs.fish;
+
+          editors = with pkgs; [
+            nano
+            helix
+            vim
+          ];
+
+          git = pkgs.gitFull;
+
+          programs = with pkgs; [
+            htop
+            btop
+            direnv
+          ];
+
+          tools = with pkgs; [
+            tree
+          ];
+
+          default-packages = [
+            shell
+            git
+          ]
+          ++ editors
+          ++ programs
+          ++ tools;
+
+          minimal-packages =
+            with pkgs;
+            [
+              helix
+              htop
+              git
+              direnv
+              tree
+            ]
+            ++ [ shell ]
+            ++ tools;
+
+          mkShell =
+            name: packages:
+            pkgs.mkShell {
+              name = "baseshell-${name}";
+              inherit packages version;
+              shellHook = ''
+                echo "Thank you for using baseshell! (https://github.com/TureBentzin/baseshell.git)"
+              '';
+            };
+
+        in
+        {
+          devShells = {
+            default = mkShell "default" default-packages;
+            minimal = mkShell "minimal" minimal-packages;
+          };
+        };
+      flake = {
+      };
+    };
+}
